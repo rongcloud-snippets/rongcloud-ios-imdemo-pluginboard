01f73247db810496fca98d4455e017d0ff839c12
wangjue <wangjue@rongcloud.cn>
2021-04-06 10:59:34 +0800

    适配 2.x 版本

--- rongcloud-ios-imdemo-pluginboard/RCSConfig.h
+++ rongcloud-ios-imdemo-pluginboard/RCSConfig.h
@@ -9,12 +9,17 @@
 #ifndef RCSConfig_h
 #define RCSConfig_h
 
+#error 填入 Appkey，填入之后删除此行
 // AppKey 获取地址 ： https://developer.rongcloud.cn/app/appkey/0vMK99Huzz-qw40Ybv4NDA
 // 选择 AppKey 需注意选中项目及选中环境，不同环境的 AppKey 不相同。
 #define RONGCLOUD_APPKEY @""
 
+#error 填入 Token，填入之后删除此行
 // 连接融云服务，需要传入对应的 token。 测试情况 token 可直接在开发者后台获取。
 // 获取 token ：https://developer.rongcloud.cn/apitool/Mw8EsJmV43kZBugTMSAZXg
 #define RONGCLOUD_TOKEN  @""
 
 #endif /* RCSConfig_h */
+
+//编译时会执行脚本，通过 podfile 文件识别 SDK 版本
+#define SDK_VERSION 4
